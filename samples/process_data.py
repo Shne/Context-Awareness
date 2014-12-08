aa8d91fc246a931aa92cfc12a10ddfcbdc9f86b3
Jan H. Knudsen <jan.h.knudsen@gmail.com>
2014-12-08 18:11:50 +0100

    fix python script so it wont window acress processed files

--- samples/process_data.py
+++ samples/process_data.py
@@ -8,25 +8,27 @@ import fileinput
 ############
 ### VARS ###
 ############
-data = []
-windowSize = 128
+dataDict = dict()
+windowSize = 256
 
 #######################
 ### PARSE this shit ###
 #######################
-regex = r'ACCELEROMETER::t=(\d+);sT=\d+;cT=\d+;x=(-?\d*\.?\d+);y=(-?\d*\.?\d+);z=(-?\d*\.?\d+)'
+regex = r'ACCELEROMETER::t=\d+;sT=\d+;cT=(\d+);x=(-?\d*\.?\d+);y=(-?\d*\.?\d+);z=(-?\d*\.?\d+)'
 
 for line in fileinput.input():
 	if(fileinput.isfirstline()):
+		filename = fileinput.filename()
+		dataDict[filename] = []
 		dataClass = re.search(r'_([^_]+)\.log$', fileinput.filename()).group(1)
-# for line in sys.stdin:
+
 	match = re.search(regex, line)
 	if(match):
-		# time = int(match.group(1))
+		time = int(match.group(1))
 		x = float(match.group(2))
 		y = float(match.group(3))
 		z = float(match.group(4))
-		data.append((x, y, z, dataClass))
+		dataDict[filename].append((x, y, z, dataClass, time))
 
 print("@RELATION HURRR")
 print("@ATTRIBUTE min NUMERIC")
@@ -35,11 +37,13 @@ print("@ATTRIBUTE dev NUMERIC")
 print("@ATTRIBUTE class {walking,running}")
 print("@DATA")
 
-for window in [data[i:i+windowSize] for i in range(0, len(data)-windowSize, int(windowSize/2))]:
-	eucNorms = [math.sqrt(values[0]*values[0] + values[1]*values[1] + values[2]*values[2]) for values in window]
-		
-	eucMin = min(eucNorms)
-	eucMax = max(eucNorms)
-	eucDev = statistics.stdev(eucNorms)
+cutlines = 0
+for (_, data) in dataDict.items():
+	for window in [data[i:i+windowSize] for i in range(cutlines, len(data)-windowSize-cutlines, int(windowSize/2))]:
+		eucNorms = [math.sqrt(values[0]*values[0] + values[1]*values[1] + values[2]*values[2]) for values in window]
 
-	print(str(eucMin)+','+str(eucMax)+','+str(eucDev)+','+str(window[0][3]))
+		eucMin = min(eucNorms)
+		eucMax = max(eucNorms)
+		eucDev = statistics.stdev(eucNorms)
+
+		print(str(eucMin)+','+str(eucMax)+','+str(eucDev)+','+str(window[0][3]))
