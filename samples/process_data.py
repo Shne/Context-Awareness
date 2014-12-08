c60243d17aee11878eba593d0131b2b664896224
Jan H. Knudsen <jan.h.knudsen@gmail.com>
2014-12-08 15:33:30 +0100

    python script now takes list of files as input and will classify lines from that file as the the last part of the name after _ before .log

--- samples/process_data.py
+++ samples/process_data.py
@@ -2,6 +2,8 @@ import sys
 import re
 import statistics
 import math
+import fileinput
+
 
 ############
 ### VARS ###
@@ -14,21 +16,23 @@ windowSize = 128
 #######################
 regex = r'ACCELEROMETER::t=(\d+);sT=\d+;cT=\d+;x=(-?\d*\.?\d+);y=(-?\d*\.?\d+);z=(-?\d*\.?\d+)'
 
-for line in sys.stdin:
+for line in fileinput.input():
+	if(fileinput.isfirstline()):
+		dataClass = re.search(r'_([^_]+)\.log$', fileinput.filename()).group(1)
+# for line in sys.stdin:
 	match = re.search(regex, line)
 	if(match):
 		# time = int(match.group(1))
 		x = float(match.group(2))
 		y = float(match.group(3))
 		z = float(match.group(4))
-		data.append((x, y, z))
-
+		data.append((x, y, z, dataClass))
 
 print("@RELATION HURRR")
 print("@ATTRIBUTE min NUMERIC")
 print("@ATTRIBUTE max NUMERIC")
 print("@ATTRIBUTE dev NUMERIC")
-print("@ATTRIBUTE class {Walking,Running}")
+print("@ATTRIBUTE class {walking,running}")
 print("@DATA")
 
 for window in [data[i:i+windowSize] for i in range(0, len(data)-windowSize, int(windowSize/2))]:
@@ -38,4 +42,4 @@ for window in [data[i:i+windowSize] for i in range(0, len(data)-windowSize, int(
 	eucMax = max(eucNorms)
 	eucDev = statistics.stdev(eucNorms)
 
-	print(str(eucMin)+','+str(eucMax)+','+str(eucDev)+',Running')
+	print(str(eucMin)+','+str(eucMax)+','+str(eucDev)+','+str(window[0][3]))
