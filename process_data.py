90733a607212e7043f68af44a096a96a08f4e1e5
Jan H. Knudsen <jan.h.knudsen@gmail.com>
2014-12-08 14:47:20 +0100

    python script for processing acc data for weka

---
+++ process_data.py
@@ -0,0 +1,41 @@
+import sys
+import re
+import statistics
+import math
+
+############
+### VARS ###
+############
+data = []
+windowSize = 128
+
+#######################
+### PARSE this shit ###
+#######################
+regex = r'ACCELEROMETER::t=(\d+);sT=\d+;cT=\d+;x=(-?\d*\.?\d+);y=(-?\d*\.?\d+);z=(-?\d*\.?\d+)'
+
+for line in sys.stdin:
+	match = re.search(regex, line)
+	# time = int(match.group(1))
+	x = float(match.group(2))
+	y = float(match.group(3))
+	z = float(match.group(4))
+	data.append((x, y, z))
+
+
+# print("@RELATION HURRR")
+# print("@ATTRIBUTE min NUMERIC")
+# print("@ATTRIBUTE max NUMERIC")
+# print("@ATTRIBUTE dev NUMERIC")
+# print("@ATTRIBUTE class {Walking,Running}")
+# print("@DATA")
+# print("min,max,dev\n")
+
+for window in [data[i:i+windowSize] for i in range(0, len(data)-windowSize, int(windowSize/2))]:
+	eucNorms = [math.sqrt(values[0]*values[0] + values[1]*values[1] + values[2]*values[2]) for values in window]
+		
+	eucMin = min(eucNorms)
+	eucMax = max(eucNorms)
+	eucDev = statistics.stdev(eucNorms)
+
+	print(str(eucMin)+','+str(eucMax)+','+str(eucDev)+',Running')
